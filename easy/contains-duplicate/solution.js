ea6a2ea773bc6eff9c9585355ef23db2df1fdcc8
Chris Schuhmacher <chris.r.schuhmacher@gmail.com>
2025-10-05 15:13:36 -0700

    implement contains duplicate

--- easy/contains-duplicate/solution.js
+++ easy/contains-duplicate/solution.js
@@ -2,8 +2,31 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+// function containsDuplicate(nums) {
+//   // Your code here using Map
+//   let seen = new Map();
+
+//   for (const num of nums) {
+//     if (seen.has(num)) return true;
+
+//     seen.set(num, true);
+//   }
+  
+//   return false;
+// }
+
 function containsDuplicate(nums) {
-    // Your code here
+  if (nums.length < 2) return false;
+  // using set
+  const seen = new Set();
+
+  for (const num of nums) {
+    if (seen.has(num)) return true;
+
+    seen.add(num);
+  }
+
+  return false;
 }
 
 module.exports = containsDuplicate;
