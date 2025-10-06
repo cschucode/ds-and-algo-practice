7810673d2510721d6566b3d2ab94914561a6e54b
Chris Schuhmacher <chris.r.schuhmacher@gmail.com>
2025-10-05 20:46:32 -0700

    Implement valid parentheses

--- easy/valid-parentheses/solution.js
+++ easy/valid-parentheses/solution.js
@@ -3,7 +3,29 @@
  * @return {boolean}
  */
 function isValid(s) {
-    // Your code here
+  const stack = [];
+  const bracketMap = { ')': '(', '}': '{', ']': '[' };
+
+  for (let i = 0; i < s.length; i++) {
+    const bracket = s[i];
+
+    // push opening brackets on stack
+    if (!bracketMap[bracket]) {
+      stack.push(bracket);
+    } else {
+      const top = stack[stack.length - 1];
+
+      // if bracket closes top of stack bracket, remove top
+      if (bracketMap[bracket] === top) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  // valid parentheses will have no brackets left and should return true. Otherwise, return false
+  return stack.length === 0;
 }
 
 module.exports = isValid;
